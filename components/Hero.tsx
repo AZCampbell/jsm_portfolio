632a9e63bd9e5d08189214fd6a85da6f72186949
Jonathan Campbell <jpcampbell1127@gmail.com>
2024-05-22 10:32:45 -0700

    Update: Hero section completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Spotlight } from "./ui/spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerate-Effect";
 import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -18,12 +19,12 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-50vh]" fill="blue" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[80vw] md:max-w-2xl lg:max-w-[60w] flex flex-col items-center justify-center">
+        <div className="max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
@@ -39,7 +40,11 @@ const Hero = () => {
 
           <a href="#about">
             {" "}
-            <MagicButton title="Show my work" icon={undefined} position={""} />
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position={"right"}
+            />
           </a>
         </div>
       </div>
